90090b7fdcea044427265ef1b93d1120cdc7c165
shchetininsrg <shchetinin.srg@yandex.ru>
2023-01-30 21:06:52 +0500

    Сделана форма авторизации

--- src/App.js
+++ src/App.js
@@ -4,8 +4,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className="App__login">
-        
+      <div className="decor_sqr"></div>
+      <div className="decor_sqr-2"></div>
+      <div className="login">
+        <h1 className="login__header">QUZIZY</h1>
+        <p className="login__text">Создай аккаунт или войди в меня <br/> Это бесплатно</p>
+        <div className="form">
+          <input type="name" placeholder='Username' className="login__input login__input_username"></input>
+          <input type="password" placeholder='Password' className="login__input login__input_password"></input>
+          <button className="login__btn">Войти</button>
+        <p className="login__text login__text_margin">Нет аккаунта? <a className="login__link" href="#">Зарегистрируйся</a></p>
+        </div>
       </div>
     </div>
   );
